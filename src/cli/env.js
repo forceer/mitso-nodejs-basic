f20495c27efdd2bc6cf400b4e0c683509e6ef85e
forceer <anton.klimchuk.2004@gmail.com>
2025-04-13 20:35:46 +0300

    cli ready

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
-const parseEnv = () => {
-    // Write your code here 
-};
 
-parseEnv();
+
+const parseEnv = () => {
+    const envs = Object.entries(process.env)
+      .filter(([key]) => key.startsWith('MITSO_'))
+      .map(([key, value]) => `${key}=${value}`);
+  
+    console.log(envs.join('; '));
+  };
+  
+  parseEnv();
+  
